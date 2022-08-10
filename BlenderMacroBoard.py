6cfc11c7abf146e31f1e069ee875e6d0119af328
SpectralVectors <robmuir1@gmail.com>
2022-08-09 22:24:24 -0400

    Add files via upload

--- BlenderMacroBoard.py
+++ BlenderMacroBoard.py
@@ -1,9 +1,9 @@
 bl_info = {
-    "name": "BlenderMacroBoard Manager",
+    "name": "BlenderMacroBoard",
     "author": "Spectral Vectors",
     "version": (0, 0, 1),
     "blender": (2, 90, 0),
-    "location": "Edit > Preferences > Addons > BlenderMacroBoard Manager",
+    "location": "Edit > Preferences > Addons > BlenderMacroBoard",
     "description": "A tool to configure your BlenderMacroBoard",
     "warning": "",
     "doc_url": "",
@@ -37,7 +37,7 @@ class AssignKey1(bpy.types.Operator):
         CategoryName = str(bmb_props.category).capitalize()
         Key1Name = str(bmb_props.operator).replace('_',' ').capitalize()
 
-        bmb_props.Key1 = f'[1] {CategoryName} - {Key1Name}'
+        bmb_props.Key1 = f'{CategoryName} - {Key1Name}'
         
         return {'FINISHED'}
 
@@ -66,7 +66,7 @@ class AssignKey2(bpy.types.Operator):
         CategoryName = str(bmb_props.category).capitalize()
         Key2Name = str(bmb_props.operator).replace('_',' ').capitalize()
 
-        bmb_props.Key2 = f'[2] {CategoryName} - {Key2Name}'
+        bmb_props.Key2 = f'{CategoryName} - {Key2Name}'
         
         return {'FINISHED'}
 
@@ -95,7 +95,7 @@ class AssignKey3(bpy.types.Operator):
         CategoryName = str(bmb_props.category).capitalize()
         Key3Name = str(bmb_props.operator).replace('_',' ').capitalize()
 
-        bmb_props.Key3 = f'[3] {CategoryName} - {Key3Name}'
+        bmb_props.Key3 = f'{CategoryName} - {Key3Name}'
         
         return {'FINISHED'}
 
@@ -124,7 +124,7 @@ class AssignKey4(bpy.types.Operator):
         CategoryName = str(bmb_props.category).capitalize()
         Key4Name = str(bmb_props.operator).replace('_',' ').capitalize()
 
-        bmb_props.Key4 = f'[4] {CategoryName} - {Key4Name}'
+        bmb_props.Key4 = f'{CategoryName} - {Key4Name}'
         
         return {'FINISHED'}
 
@@ -153,7 +153,7 @@ class AssignKey5(bpy.types.Operator):
         CategoryName = str(bmb_props.category).capitalize()
         Key5Name = str(bmb_props.operator).replace('_',' ').capitalize()
 
-        bmb_props.Key5 = f'[5] {CategoryName} - {Key5Name}'
+        bmb_props.Key5 = f'{CategoryName} - {Key5Name}'
         
         return {'FINISHED'}
 
@@ -182,7 +182,7 @@ class AssignKey6(bpy.types.Operator):
         CategoryName = str(bmb_props.category).capitalize()
         Key6Name = str(bmb_props.operator).replace('_',' ').capitalize()
 
-        bmb_props.Key6 = f'[6] {CategoryName} - {Key6Name}'
+        bmb_props.Key6 = f'{CategoryName} - {Key6Name}'
         
         return {'FINISHED'}
 
@@ -211,7 +211,7 @@ class AssignKey7(bpy.types.Operator):
         CategoryName = str(bmb_props.category).capitalize()
         Key7Name = str(bmb_props.operator).replace('_',' ').capitalize()
 
-        bmb_props.Key7 = f'[7] {CategoryName} - {Key7Name}'
+        bmb_props.Key7 = f'{CategoryName} - {Key7Name}'
         
         return {'FINISHED'}
 
@@ -240,7 +240,7 @@ class AssignKey8(bpy.types.Operator):
         CategoryName = str(bmb_props.category).capitalize()
         Key8Name = str(bmb_props.operator).replace('_',' ').capitalize()
 
-        bmb_props.Key8 = f'[8] {CategoryName} - {Key8Name}'
+        bmb_props.Key8 = f'{CategoryName} - {Key8Name}'
         
         return {'FINISHED'}
 
@@ -269,10 +269,270 @@ class AssignKey9(bpy.types.Operator):
         CategoryName = str(bmb_props.category).capitalize()
         Key9Name = str(bmb_props.operator).replace('_',' ').capitalize()
 
-        bmb_props.Key9 = f'[9] {CategoryName} - {Key9Name}'
+        bmb_props.Key9 = f'{CategoryName} - {Key9Name}'
         
         return {'FINISHED'}
 
+class AssignR1Left(bpy.types.Operator):
+    """Assign an Operator to Rotary 1 Left Turn"""
+    
+    bl_idname = "addon.assign_r1left"
+    bl_label = "Rotary 1 Left"
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+    def draw(self, context):
+
+        bmb_props = bpy.context.scene.bmb_props
+
+        layout = self.layout
+        col = layout.column()
+        col.prop(bmb_props, 'category', expand=False)
+        col.prop(bmb_props, 'operator', expand=False)
+
+    def execute(self, context):
+
+        bmb_props = context.scene.bmb_props
+
+        CategoryName = str(bmb_props.category).capitalize()
+        R1LeftName = str(bmb_props.operator).replace('_',' ').capitalize()
+
+        bmb_props.R1Left = f'{CategoryName} - {R1LeftName}'
+        
+        return {'FINISHED'}
+
+class AssignR1Push(bpy.types.Operator):
+    """Assign an Operator to Rotary 1 Push"""
+    
+    bl_idname = "addon.assign_r1push"
+    bl_label = "Rotary 1 Push"
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+    def draw(self, context):
+
+        bmb_props = bpy.context.scene.bmb_props
+
+        layout = self.layout
+        col = layout.column()
+        col.prop(bmb_props, 'category', expand=False)
+        col.prop(bmb_props, 'operator', expand=False)
+
+    def execute(self, context):
+
+        bmb_props = context.scene.bmb_props
+
+        CategoryName = str(bmb_props.category).capitalize()
+        R1PushName = str(bmb_props.operator).replace('_',' ').capitalize()
+
+        bmb_props.R1Push = f'{CategoryName} - {R1PushName}'
+        
+        return {'FINISHED'}
+
+class AssignR1Right(bpy.types.Operator):
+    """Assign an Operator to Rotary 1 Right Turn"""
+    
+    bl_idname = "addon.assign_r1right"
+    bl_label = "Rotary 1 Right"
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+    def draw(self, context):
+
+        bmb_props = bpy.context.scene.bmb_props
+
+        layout = self.layout
+        col = layout.column()
+        col.prop(bmb_props, 'category', expand=False)
+        col.prop(bmb_props, 'operator', expand=False)
+
+    def execute(self, context):
+
+        bmb_props = context.scene.bmb_props
+
+        CategoryName = str(bmb_props.category).capitalize()
+        R1RightName = str(bmb_props.operator).replace('_',' ').capitalize()
+
+        bmb_props.R1Right = f'{CategoryName} - {R1RightName}'
+        
+        return {'FINISHED'}
+
+class AssignR2Left(bpy.types.Operator):
+    """Assign an Operator to Rotary 2 Left Turn"""
+    
+    bl_idname = "addon.assign_r2left"
+    bl_label = "Rotary 2 Left"
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+    def draw(self, context):
+
+        bmb_props = bpy.context.scene.bmb_props
+
+        layout = self.layout
+        col = layout.column()
+        col.prop(bmb_props, 'category', expand=False)
+        col.prop(bmb_props, 'operator', expand=False)
+
+    def execute(self, context):
+
+        bmb_props = context.scene.bmb_props
+
+        CategoryName = str(bmb_props.category).capitalize()
+        R2LeftName = str(bmb_props.operator).replace('_',' ').capitalize()
+
+        bmb_props.R2Left = f'{CategoryName} - {R2LeftName}'
+        
+        return {'FINISHED'}
+
+class AssignR2Push(bpy.types.Operator):
+    """Assign an Operator to Rotary 2 Push"""
+    
+    bl_idname = "addon.assign_r2push"
+    bl_label = "Rotary 2 Push"
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+    def draw(self, context):
+
+        bmb_props = bpy.context.scene.bmb_props
+
+        layout = self.layout
+        col = layout.column()
+        col.prop(bmb_props, 'category', expand=False)
+        col.prop(bmb_props, 'operator', expand=False)
+
+    def execute(self, context):
+
+        bmb_props = context.scene.bmb_props
+
+        CategoryName = str(bmb_props.category).capitalize()
+        R2PushName = str(bmb_props.operator).replace('_',' ').capitalize()
+
+        bmb_props.R2Push = f'{CategoryName} - {R2PushName}'
+        
+        return {'FINISHED'}
+
+class AssignR2Right(bpy.types.Operator):
+    """Assign an Operator to Rotary 2 Right Turn"""
+    
+    bl_idname = "addon.assign_r2right"
+    bl_label = "Rotary 2 Right"
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+    def draw(self, context):
+
+        bmb_props = bpy.context.scene.bmb_props
+
+        layout = self.layout
+        col = layout.column()
+        col.prop(bmb_props, 'category', expand=False)
+        col.prop(bmb_props, 'operator', expand=False)
+
+    def execute(self, context):
+
+        bmb_props = context.scene.bmb_props
+
+        CategoryName = str(bmb_props.category).capitalize()
+        R2RightName = str(bmb_props.operator).replace('_',' ').capitalize()
+
+        bmb_props.R2Right = f'{CategoryName} - {R2RightName}'
+        
+        return {'FINISHED'}
+
+class AssignR3Left(bpy.types.Operator):
+    """Assign an Operator to Rotary 3 Left Turn"""
+    
+    bl_idname = "addon.assign_r3left"
+    bl_label = "Rotary 3 Left"
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+    def draw(self, context):
+
+        bmb_props = bpy.context.scene.bmb_props
+
+        layout = self.layout
+        col = layout.column()
+        col.prop(bmb_props, 'category', expand=False)
+        col.prop(bmb_props, 'operator', expand=False)
+
+    def execute(self, context):
+
+        bmb_props = context.scene.bmb_props
+
+        CategoryName = str(bmb_props.category).capitalize()
+        R3LeftName = str(bmb_props.operator).replace('_',' ').capitalize()
+
+        bmb_props.R3Left = f'{CategoryName} - {R3LeftName}'
+        
+        return {'FINISHED'}
+
+class AssignR3Push(bpy.types.Operator):
+    """Assign an Operator to Rotary 3 Push"""
+    
+    bl_idname = "addon.assign_r3push"
+    bl_label = "Rotary 3 Push"
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+    def draw(self, context):
+
+        bmb_props = bpy.context.scene.bmb_props
+
+        layout = self.layout
+        col = layout.column()
+        col.prop(bmb_props, 'category', expand=False)
+        col.prop(bmb_props, 'operator', expand=False)
+
+    def execute(self, context):
+
+        bmb_props = context.scene.bmb_props
+
+        CategoryName = str(bmb_props.category).capitalize()
+        R3PushName = str(bmb_props.operator).replace('_',' ').capitalize()
+
+        bmb_props.R3Push = f'{CategoryName} - {R3PushName}'
+        
+        return {'FINISHED'}
+
+class AssignR3Right(bpy.types.Operator):
+    """Assign an Operator to Rotary 3 Right Turn"""
+    
+    bl_idname = "addon.assign_r3right"
+    bl_label = "Rotary 3 Right"
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+    def draw(self, context):
+
+        bmb_props = bpy.context.scene.bmb_props
+
+        layout = self.layout
+        col = layout.column()
+        col.prop(bmb_props, 'category', expand=False)
+        col.prop(bmb_props, 'operator', expand=False)
+
+    def execute(self, context):
+
+        bmb_props = context.scene.bmb_props
+
+        CategoryName = str(bmb_props.category).capitalize()
+        R3RightName = str(bmb_props.operator).replace('_',' ').capitalize()
+
+        bmb_props.R3Right = f'{CategoryName} - {R3RightName}'
+        
+        return {'FINISHED'}
 
 class BlenderMacroBoardProperties(bpy.types.PropertyGroup):
 
@@ -445,17 +705,17 @@ class BlenderMacroBoardPreferences(bpy.types.AddonPreferences):
         bmb_props = bpy.context.scene.bmb_props
 
         layout = self.layout
-        box = layout.box()
-        box.label(icon='BLENDER', text='BlenderMacroBoard Manager:')
-        box2=box.box()
-        box2.label(text='Presets:', icon='DOCUMENTS')
-        row=box2.row()
+        row = layout.row()
+        row.label(icon='BLENDER', text='BlenderMacroBoard:')
+        box=layout.box()
+        box.label(text='Presets:', icon='DOCUMENTS')
+        row=box.row()
         row.prop(bmb_props, 'Page', icon='TEXT')
         row.prop(bmb_props, 'Preset', icon='PRESET')
-        row=box2.row(align=True)
+        row=box.row(align=True)
         row.operator(AssignKey1.bl_idname, text='Save Current Page as Preset', icon='PRESET_NEW')
         row.operator(AssignKey1.bl_idname, text='Load Preset from File', icon='FILEBROWSER')
-        box=box.box()        
+        box=layout.box()        
         column = box.column(align=True)
         column.label(text='Customize Individual Keys:', icon='TOOL_SETTINGS')
         column.label(text='Keypad:', icon='VIEW_ORTHO')
@@ -473,19 +733,19 @@ class BlenderMacroBoardPreferences(bpy.types.AddonPreferences):
         row.operator(AssignKey9.bl_idname, text=bmb_props.Key9)
         column.label(text='Rotary 1:', icon='FILE_REFRESH')
         row = column.row(align=True)
-        row.operator(AssignKey1.bl_idname, text=bmb_props.R1Left, icon='LOOP_BACK')
-        row.operator(AssignKey1.bl_idname, text=bmb_props.R1Push, icon='SORT_ASC')
-        row.operator(AssignKey1.bl_idname, text=bmb_props.R1Right, icon='LOOP_FORWARDS')
+        row.operator(AssignR1Left.bl_idname, text=bmb_props.R1Left, icon='LOOP_BACK')
+        row.operator(AssignR1Push.bl_idname, text=bmb_props.R1Push, icon='SORT_ASC')
+        row.operator(AssignR1Right.bl_idname, text=bmb_props.R1Right, icon='LOOP_FORWARDS')
         column.label(text='Rotary 2:', icon='FILE_REFRESH')
         row = column.row(align=True)
-        row.operator(AssignKey1.bl_idname, text=bmb_props.R2Left, icon='LOOP_BACK')
-        row.operator(AssignKey1.bl_idname, text=bmb_props.R2Push, icon='SORT_ASC')
-        row.operator(AssignKey1.bl_idname, text=bmb_props.R2Right, icon='LOOP_FORWARDS')
+        row.operator(AssignR2Left.bl_idname, text=bmb_props.R2Left, icon='LOOP_BACK')
+        row.operator(AssignR2Push.bl_idname, text=bmb_props.R2Push, icon='SORT_ASC')
+        row.operator(AssignR2Right.bl_idname, text=bmb_props.R2Right, icon='LOOP_FORWARDS')
         column.label(text='Rotary 3:', icon='FILE_REFRESH')
         row = column.row(align=True)
-        row.operator(AssignKey1.bl_idname, text=bmb_props.R3Left, icon='LOOP_BACK')
-        row.operator(AssignKey1.bl_idname, text=bmb_props.R3Push, icon='SORT_ASC')
-        row.operator(AssignKey1.bl_idname, text=bmb_props.R3Right, icon='LOOP_FORWARDS')
+        row.operator(AssignR3Left.bl_idname, text=bmb_props.R3Left, icon='LOOP_BACK')
+        row.operator(AssignR3Push.bl_idname, text=bmb_props.R3Push, icon='SORT_ASC')
+        row.operator(AssignR3Right.bl_idname, text=bmb_props.R3Right, icon='LOOP_FORWARDS')
 
 
 
@@ -501,7 +761,15 @@ classes = [
     AssignKey7,
     AssignKey8,
     AssignKey9,
-
+    AssignR1Left,
+    AssignR1Push,
+    AssignR1Right,
+    AssignR2Left,
+    AssignR2Push,
+    AssignR2Right,
+    AssignR3Left,
+    AssignR3Push,
+    AssignR3Right,
 ]
 
 def register():
